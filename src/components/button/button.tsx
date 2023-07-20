6fdaaa418fe325f446c6bf1fca3cfaf764180038
Tri Nguyen <tringuyen9295@gmail.com>
2023-07-21 00:37:57 +1000

    add disabled state

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -5,6 +5,7 @@ interface Props {
 	text: string | React.ReactNode;
 	type?: ButtonType;
 	smallPadding?: boolean;
+	disabled?: boolean;
 	onClick: () => void;
 }
 
@@ -12,20 +13,29 @@ export const Button = ({
 	text,
 	type = ButtonType.PRIMARY,
 	onClick,
-	...props
+	smallPadding = false,
+	disabled = false,
 }: Props) => {
+	const handleClick = () => {
+		if (disabled) return;
+
+		onClick();
+	};
+
 	return (
 		<button
 			className={clsx(
-				"w-fit h-8 border border-solid rounded",
-				props.smallPadding ? "px-2" : "px-6",
+				"h-8 w-fit rounded border border-solid",
+				smallPadding ? "px-2" : "px-6",
 				type === ButtonType.PRIMARY &&
 					"bg-green-300 hover:bg-green-400",
 				type === ButtonType.ERROR && "bg-red-300 hover:bg-red-400",
 				type === ButtonType.INVERT &&
 					"border-green-300 bg-white hover:bg-green-300 hover:text-white",
+				disabled &&
+					"cursor-not-allowed bg-slate-300 hover:bg-slate-400",
 			)}
-			onClick={onClick}
+			onClick={handleClick}
 		>
 			{text}
 		</button>
